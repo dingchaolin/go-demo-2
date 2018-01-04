4b2ac35ed3c8dd42fb490d71fae1d7fffba9b661
dingchaolin <18810042351@163.com>
2018-01-04 21:32:46 +0800

    feat:reader

---
+++ src/demo6/reader.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+	"compress/gzip"
+	"log"
+)
+
+type MyReader struct{
+	r io.Reader
+	cnt int
+}
+
+func NewMyReader( r io.Reader ) *MyReader{
+	return &MyReader{
+		r : r,
+	}
+}
+func ( r *MyReader ) Read( b []byte )( int, error){
+	n ,err := r.r.Read( b )
+	r.cnt += n
+	return n, err
+}
+
+
+func main(){
+	uncompress, err := gzip.NewReader( os.Stdin )
+	if err != nil {
+		log.Fatal( err )
+	}
+
+	//r := NewMyReader( os.Stdin )
+	r := NewMyReader( uncompress )
+
+	io.Copy( os.Stdout, r )
+	fmt.Println( "size===", r.cnt )
+}
+
