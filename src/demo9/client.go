85ec10c3a26bfe4449c8649641a42dcf675b3184
dingchaolin <18810042351@163.com>
2018-01-11 23:46:47 +0800

    feat: socket client

---
+++ src/demo9/client.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net"
+	"log"
+	"fmt"
+	"io"
+	"os"
+)
+// tcp 是双工的 任何一方都可以主动发起关闭  关闭之后 另一方会收到 EOF
+func main(){
+	addr := "www.baidu.com:80"
+	//拨号
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println( conn.RemoteAddr().String() )
+	fmt.Println( conn.LocalAddr().String() )
+
+	n, err := conn.Write([]byte("GET / HTTP/1.1\r\n\r\n"))
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println("write size  ", n )
+
+	//buf := make([]byte, 4096)
+	//buf := make([]byte, 10)
+	//n, err = conn.Read(buf)//读写都是流式数据
+	///*
+	//EOF 在网络编程中 表示对方关闭了链接 唯一判断对方关闭了链接
+	// */
+	//if err != nil && err != io.EOF{
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(string(buf[:n]))
+	io.Copy( os.Stdout, conn)
+
+	conn.Close()
+}
+
+// dig baidu.com 查看所有的dns ip
+
--- src/demo9/pool_step.go
+++ src/demo9/pool_step.go
@@ -4,9 +4,12 @@ import (
 	"net/http"
 	"log"
 	"fmt"
-	"time"
+	"sync"
 )
 
+/*
+适合短小的批量任务
+ */
 // 给定一个url返回的url的status
 // www.baidu.com 200 OK
 func printUrl( url string){
@@ -19,7 +22,8 @@ func printUrl( url string){
 	fmt.Println( url, resp.Status)
 }
 
-func _work( ch chan string ){
+func _work( ch chan string, wg * sync.WaitGroup ){
+	defer wg.Done()//done会让计数器减一
 	/*
 		for{
 			url,ok  := <- ch
@@ -42,25 +46,42 @@ func _work( ch chan string ){
 // 2. 如果channel里面没有数据 接收方会阻塞
 // 3. 如果没有人正在等待channel的数据 发送方会阻塞
 // 4. 从一个close的channel取数据永远不会则是 同时获取的是默认值
+// 5. 一个channel中的数据是可以被多个协程竞争获取的
 // 主协程启动一个work协程 同时传递一个channel
 // 主协程向channel里面发送一个url
 // work协程从channel里面获取url 之后调用printUrl打印url
 // work一直工作直到channel关闭
-
+// 永远不要使用sleep的方式来进行协程同步
 //启动多个work协程
 // 主协程向channel里面发送多个url 之后调用printUrl打印url
 //work携程不停重复第三条 指导channel关闭
+
+//创建一个WaitGroup
+//调用add
+//调用Wait等待携程结束
+
 func main(){
  ch := make(chan string)//channel是一个动态数组 数组长度随时都在变化
+ wg := new (sync.WaitGroup )
+ // add 的次数 跟 调用done的次数要一样
+ // waitgroup 只管次数 不管对应关系
+ wg.Add( 3 ) //先add为好 add会让计数器家加数
  for i := 0; i < 3; i ++{
-	 go _work(ch)
+	 go _work(ch, wg )
  }
-
+ // 起一个协程 add(1) 跟上面的效果相同
+/*
+	for i := 0; i < 3; i ++{
+		wg.Add( 1 )
+		go _work(ch, wg )
+	}
+*/
  for i:= 0; i < 5; i ++ {
 	 url := "http://www.baidu.com"
 	 ch  <- url
  }
  //在生产者中关闭通道
  close(ch)
- time.Sleep( time.Second * 3)
+ wg.Wait()
+ //time.Sleep( time.Second * 3)
 }
