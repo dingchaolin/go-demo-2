7a334bdc005e0ed2226348183260564a42425b92
dingchaolin <18810042351@163.com>
2018-02-12 10:26:57 +0800

    feat: elastic.v5

--- src/demo13/saver/saver.go
+++ src/demo13/saver/saver.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
 	cluster "github.com/bsm/sarama-cluster"
 	"log"
 	"github.com/Shopify/sarama"
+	elastic "gopkg.in/olivere/elastic.v5"
+	"time"
 )
+
+func indexName()string{
+	date := time.Now().Format("20160102")
+	return "topic-" + date
+}
+
 func main(){
 	config := cluster.NewConfig()
 	config.Group.Mode = cluster.ConsumerModePartitions
@@ -14,10 +23,23 @@ func main(){
 	if err != nil && ok{
 		log.Fatal( err )
 	}
+
+	esclient, err := elastic.NewClient( elastic.SetURL("http://127.0.0.1:9200"))
+	if err != nil {
+		log.Fatal( err )
+	}
+
 	for{
 		select {
 			case msg := <-partition.Messages():
 				log.Print(string(msg.Value), msg.Offset)
+
+				// elasticsearch写入
+				// 所有 indexname type 必须全小写 可以有- _ 等字符
+				_, err := esclient.Index().Index(indexName()).Type("topic").BodyString(string(msg.Value)).Do(context.TODO())
+				if err != nil {
+					log.Fatal(err)
+				}
 			case err := <-partition.Errors():
 				log.Print(err)
 		}
